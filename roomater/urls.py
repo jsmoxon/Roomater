c705e8accd05129ead063e4831c6c57719adf95e
Jack Moxon <jsmoxon@gmail.com>
2012-02-17 16:33:10 -0800

    added heroku reqs

--- roomater/urls.py
+++ roomater/urls.py
@@ -19,3 +19,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve', kwargs={"insecure": True}),
+)
