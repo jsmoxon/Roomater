efe2aff9cc553f12d797dee7df107ed80d3c6fc5
Jack Moxon <jsmoxon@gmail.com>
2012-03-26 18:13:57 -0700

    added spaces to maps.py

--- roomater/backend/maps.py
+++ roomater/backend/maps.py
@@ -8,7 +8,7 @@ api_address_1 = "&sensor=false"
 zap = "http://api.zappos.com/Search/term/mens%20pants?limit=100&page=1&key=cf3dffea27e37c3371ee725fa5f19bea2c23e5de"
 
 def geo_code(address, city, state, zip):
-    link = str(api_address_0+address+city+state+zip+api_address_1)
+    link = str(api_address_0+address+" "+city+" "+state+" "+zip+api_address_1)
     open = urllib.urlopen(link)
     result = json.load(open)
     try:
